1b9ffb48de46f2229aa77fad14d2c0f35bb16b77
Ryan Hyma <ryan.hyma@gmail.com>
2019-12-02 15:19:44 -0500

    Added support for french/english

--- EPRequest/src/main/java/ca/tbssct/ep/EpRequestApplication.java
+++ EPRequest/src/main/java/ca/tbssct/ep/EpRequestApplication.java
@@ -1,13 +1,16 @@
 package ca.tbssct.ep;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
 
 @SpringBootApplication
+@EnableConfigurationProperties(ConfigProperties.class)
 public class EpRequestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EpRequestApplication.class, args);
 	}
+	
 
 }
