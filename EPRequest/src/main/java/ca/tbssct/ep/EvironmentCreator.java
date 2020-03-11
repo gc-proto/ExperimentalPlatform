016c696138a424816a2a5c03fe4967625c5bd8db
Ryan Hyma <ryan.hyma@gmail.com>
2020-03-11 10:50:17 -0400

    Updates

--- EPRequest/src/main/java/ca/tbssct/ep/EvironmentCreator.java
+++ EPRequest/src/main/java/ca/tbssct/ep/EvironmentCreator.java
@@ -30,14 +30,14 @@ public class EvironmentCreator {
 		if (!publicIP.equals("null")) {
 			// now use the command line to add a DNS entry using the azure command line.
 			logger.info(EvironmentCreator.this.ExecuteCommand(HELM_SCRIPTS,
-					"az network dns record-set a add-record -g DNSZone -z " + Util.GetHost()+ " -n " + instanceName
-							 + " -a " + publicIP));
+					"az network dns record-set a add-record -g DNSZone -z " + Util.GetHost() + " -n " + instanceName
+							+ " -a " + publicIP));
 			// check that the DNS record is available.
 			boolean keepGoing = true;
 			int count = 0;
 			while (keepGoing) {
 				String response = EvironmentCreator.this.ExecuteCommand(HELM_SCRIPTS,
-						"nslookup " + instanceName + "."+Util.GetHost());
+						"nslookup " + instanceName + "." + Util.GetHost());
 				logger.info(response);
 				if (response.contains(publicIP)) {
 					logger.info("DNS entry found. Confirmation will be sent");
@@ -125,7 +125,7 @@ public class EvironmentCreator {
 	}
 
 	public boolean createNamespace(String instanceName) {
-		String output = EvironmentCreator.this.ExecuteCommand(HELM_SCRIPTS, "kubectl create namespace " + instanceName);
+		String output = EvironmentCreator.this.ExecuteCommand(HELM_SCRIPTS, "kubectl create namespace " + instanceName+"-drupal");
 		if (output.toUpperCase().contains("ERROR") && !output.toUpperCase().contains("ALREADY EXISTS")) {
 			Util.handleError(output, instanceName, logger);
 			return false;
@@ -183,9 +183,9 @@ public class EvironmentCreator {
 		content = content.replace("##publicShare##", instanceName + "-drupal-public");
 		content = content.replace("##privateShare##", instanceName + "-drupal-private");
 		content = content.replace("##themesShare##", instanceName + "-drupal-themes");
-		content = content.replace("##host##", instanceName +"."+ Util.GetHost());
+		content = content.replace("##host##", instanceName + "." + Util.GetHost());
 		content = content.replace("##hostSecret##",
-				(instanceName +"." +Util.GetHost().toLowerCase().replace(".", "-") + "-tls-secret"));
+				((instanceName + "." + Util.GetHost()).toLowerCase().replace(".", "-") + "-tls-secret"));
 		Util.writeFile(path, content);
 	}
 
