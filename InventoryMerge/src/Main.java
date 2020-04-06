809a79213832e121d9ac185f400e4f07d996ffc5
Ryan Hyma <ryan.hyma@gmail.com>
2020-04-06 13:48:13 -0400

    Headers translation

--- InventoryMerge/src/Main.java
+++ InventoryMerge/src/Main.java
@@ -90,6 +90,7 @@ public class Main {
 
 	public static void main(String args[]) throws Exception {
 		Main main = new Main();
+		main.dumpHeaders();
 		main.loadThemes();
 		main.loadDepartments();
 		main.loadData();
@@ -97,6 +98,22 @@ public class Main {
 		main.outputDataHTML("en");
 		main.outputDataHTML("fr");
 		// main.outputURLMatch();
+	}
+	
+	public void dumpHeaders() throws Exception {
+		final BufferedWriter writer = Files.newBufferedWriter(Paths.get("./export/headers.csv"));
+		final CSVFormat csvFormat = CSVFormat.EXCEL.withHeader("English","French");
+		try (CSVPrinter csvPrinter = new CSVPrinter(writer, csvFormat);) {
+			for (String header: MERGED_HEADERS) {
+				csvPrinter.printRecord(header,"");
+			}
+			for (String header: SEARCH_HEADERS) {
+				csvPrinter.printRecord(header,"");
+			}
+			for (String header: AEM_HEADER) {
+				csvPrinter.printRecord(header,"");
+			}
+		}
 	}
 
 	public void mergeData() {
