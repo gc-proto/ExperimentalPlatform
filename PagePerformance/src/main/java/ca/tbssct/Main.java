4d3ba27b0779bfd5a5b212353e9728e572478ca7
Ryan Hyma <ryan.hyma@gmail.com>
2020-05-06 11:36:06 -0400

    Updates to caching algorithm

--- PagePerformance/src/main/java/ca/tbssct/Main.java
+++ PagePerformance/src/main/java/ca/tbssct/Main.java
@@ -1,4 +1,5 @@
 package ca.tbssct;
+
 import java.net.URL;
 import java.net.URLEncoder;
 import java.text.SimpleDateFormat;
@@ -15,24 +16,20 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import com.machinepublishers.jbrowserdriver.JBrowserDriver;
-import com.machinepublishers.jbrowserdriver.Settings;
-import com.machinepublishers.jbrowserdriver.Timezone;
-import com.machinepublishers.jbrowserdriver.UserAgent;
 
 public class Main {
 
 	private HashSet<String> links = new HashSet<String>();
 	private SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
 	private String today;
+	private String yesterday;
 	private String sevenDaysAgo;
 	private String thirtyDaysAgo;
 	public static long WAIT_TIME = 10000;
 	public static long FIVE_MINUTES = 1000 * 60 * 5;
-	public static final String PAGE_PERFORMANCE_URL = "http://pageperformance-nginx/";
+	// public static final String PAGE_PERFORMANCE_URL =
+	// "http://pageperformance-nginx/php/process-cj.php";
+	public static final String PAGE_PERFORMANCE_URL = "https://pageperformance.alpha.canada.ca/php/process-cj.php";
 	public static int NUM_THREADS = 6;
 	private ExecutorService executor = null;
 	public static AtomicInteger numCached = new AtomicInteger(0);
@@ -56,9 +53,9 @@ public class Main {
 	}
 
 	public void cachePages() {
-		this.cachePages(thirtyDaysAgo, today);
-		this.cachePages(sevenDaysAgo, today);
-		this.cachePages(today, today);
+		// this.cachePages(thirtyDaysAgo, today);
+		// this.cachePages(sevenDaysAgo, today);
+		this.cachePages(yesterday, today);
 	}
 
 	public void cachePages(String from, String to) {
@@ -90,52 +87,32 @@ public class Main {
 		}
 
 		public void run() {
-			WebDriver driver = new JBrowserDriver(Settings.builder().timezone(Timezone.AMERICA_NEWYORK)
-					.userAgent(UserAgent.CHROME).javascript(true).build());
 			try {
 				URL url = new URL(PAGE_PERFORMANCE_URL + "?url=" + URLEncoder.encode(pageURL, "UTF-8") + "&start="
 						+ from + "&end=" + to);
-
-				driver.get(url.toString());
-				boolean keepGoing = true;
-				long startTime = System.currentTimeMillis();
-				long maxTime = 10 * 60 * 1000;
-				while (keepGoing) {
-					WebElement loading = driver.findElement(By.cssSelector("#loading"));
-					if (!loading.isDisplayed()) {
-						keepGoing = false;
-						System.out.println("Num:" + numCached.incrementAndGet() + " Cached page: Code:"
-								+ driver.getTitle() + " URL:" + url.toString());
-
-					} else {
-						System.out.println(loading.getText() + " Caching page: " + url.toString() + " From: " + from
-								+ " To: " + to);
-						Thread.sleep(10000);
-						long difference = System.currentTimeMillis() - startTime;
-						if (difference >= maxTime) {
-							System.out.println("Gave Up:" + pageURL + " : " + difference / 1000 / 60);
-							keepGoing = false;
-						}
-					}
-				}
-
+				System.out.println("Caching: " + pageURL);
+				Document doc = Jsoup.connect(url.toString()).timeout(5 * 60 * 1000).get();
+				System.out.println(doc.outerHtml());
 			} catch (Exception e) {
 				System.out.println(e.getMessage());
-			} finally {
-				driver.quit();
 			}
-
 		}
-
 	}
 
 	public void calculateDates() {
 		this.today = format.format(new Date());
-		this.sevenDaysAgo = this.calculateDate(-7);
-		this.thirtyDaysAgo = this.calculateDate(-30);
+		this.yesterday = this.calculateDays(-1);
+		this.sevenDaysAgo = this.calculateDays(-7);
+		this.thirtyDaysAgo = this.calculateDays(-30);
 	}
 
-	public String calculateDate(int days) {
+	public String calculateMonth(int month) {
+		Calendar cal = Calendar.getInstance();
+		cal.add(Calendar.MONTH, month);
+		return format.format(cal.getTime());
+	}
+
+	public String calculateDays(int days) {
 		Calendar cal = Calendar.getInstance();
 		cal.add(Calendar.DAY_OF_MONTH, days);
 		return format.format(cal.getTime());
