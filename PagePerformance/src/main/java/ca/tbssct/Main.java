f22e2296e68985d2d67ee3cc926777c03613ebf6
Ryan Hyma <ryan.hyma@gmail.com>
2020-05-25 11:47:12 -0400

    Update to support new API of pageperformance, dates no longer needed

--- PagePerformance/src/main/java/ca/tbssct/Main.java
+++ PagePerformance/src/main/java/ca/tbssct/Main.java
@@ -2,10 +2,7 @@ package ca.tbssct;
 
 import java.net.URL;
 import java.net.URLEncoder;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
@@ -22,11 +19,6 @@ import org.jsoup.select.Elements;
 public class Main {
 
 	private HashSet<String> links = new HashSet<String>();
-	private SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-	private String today;
-	private String yesterday;
-	private String sevenDaysAgo;
-	private String thirtyDaysAgo;
 	public static long WAIT_TIME = 10000;
 	public static long FIVE_MINUTES = 1000 * 60 * 5;
 	// public static final String PAGE_PERFORMANCE_URL =
@@ -39,8 +31,6 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 
 		Main main = new Main();
-		main.setupDriver();
-		main.calculateDates();
 		main.downloadPageList();
 		main.cachePages();
 		main.cachePages();
@@ -51,20 +41,10 @@ public class Main {
 				new LinkedBlockingQueue<Runnable>());
 	}
 
-	public void setupDriver() {
-
-	}
-
 	public void cachePages() {
-		this.cachePages(sevenDaysAgo, today);
-		this.cachePages(thirtyDaysAgo, today);
-		this.cachePages(yesterday, today);
-	}
-
-	public void cachePages(String from, String to) {
 		for (String url : links) {
 			if (url.contains("www.canada.ca")) {
-				Runnable cacher = new PageCacher(url, from, to);
+				Runnable cacher = new PageCacher(url);
 				try {
 					this.executor.execute(cacher);
 				} catch (Exception e) {
@@ -84,20 +64,15 @@ public class Main {
 
 	public static class PageCacher implements Runnable {
 
-		String from;
-		String to;
 		String pageURL;
 
-		public PageCacher(String pageURL, String from, String to) {
-			this.from = from;
-			this.to = to;
+		public PageCacher(String pageURL) {
 			this.pageURL = pageURL;
 		}
 
 		public void run() {
 			try {
-				URL url = new URL(PAGE_PERFORMANCE_URL + "?url=" + URLEncoder.encode(pageURL, "UTF-8") + "&start="
-						+ from + "&end=" + to);
+				URL url = new URL(PAGE_PERFORMANCE_URL + "?url=" + URLEncoder.encode(pageURL, "UTF-8"));
 				System.out.println("Caching: " + pageURL);
 				System.out.println("Cache call:" + url.toString());
 				Document doc = Jsoup.connect(url.toString()).timeout(5 * 60 * 1000).get();
@@ -110,27 +85,9 @@ public class Main {
 		}
 	}
 
-	public void calculateDates() {
-		this.today = format.format(new Date());
-		this.yesterday = this.calculateDays(-1);
-		this.sevenDaysAgo = this.calculateDays(-7);
-		this.thirtyDaysAgo = this.calculateDays(-30);
-	}
-
-	public String calculateMonth(int month) {
-		Calendar cal = Calendar.getInstance();
-		cal.add(Calendar.MONTH, month);
-		return format.format(cal.getTime());
-	}
-
-	public String calculateDays(int days) {
-		Calendar cal = Calendar.getInstance();
-		cal.add(Calendar.DAY_OF_MONTH, days);
-		return format.format(cal.getTime());
-	}
-
 	public void downloadPageList() throws Exception {
-		Document doc = Jsoup.connect("https://covid-19inventory.tbs.alpha.canada.ca/covid19_fr.html").maxBodySize(0).get();
+		Document doc = Jsoup.connect("https://covid-19inventory.tbs.alpha.canada.ca/covid19_fr.html").maxBodySize(0)
+				.get();
 		this.links = new HashSet<String>();
 		links.addAll(this.parseDocument(doc));
 		doc = Jsoup.connect("https://covid-19inventory.tbs.alpha.canada.ca/covid19_en.html").maxBodySize(0).get();
@@ -138,14 +95,14 @@ public class Main {
 		for (String link : links) {
 			System.out.println(link);
 		}
-		System.out.println("End of links: "+links.size());
+		System.out.println("End of links: " + links.size());
 	}
 
 	public List<String> parseDocument(Document doc) {
 		List<String> list = new ArrayList<String>();
 		Element table = doc.getElementById("dataset-filter");
 		Elements elements = table.select("td:eq(2) a");
-		System.out.println("Elements: "+elements.size());
+		System.out.println("Elements: " + elements.size());
 		for (Element element : elements) {
 			String url = element.attr("href");
 			list.add(url);
