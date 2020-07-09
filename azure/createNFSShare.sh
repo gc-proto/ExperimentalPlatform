7f20316758e8f306d8fdf2652b01b3dc52ef98f6
Ryan Hyma <ryan.hyma@gmail.com>
2020-07-09 13:49:34 -0400

    Update to script for creating azure NFS share, now corectly runs as bash script

--- azure/createNFSShare.sh
+++ azure/createNFSShare.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 export AZURE_STORAGE_CONNECTION_STRING=`az storage account show-connection-string --name tbsnfs --resource-group tbs-nfs-rg -o tsv`
 echo ConnectionString:$AZURE_STORAGE_CONNECTION_STRING
 az storage share create \
